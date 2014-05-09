74714b40ed7f89b5e76ca7ae361447db4ad95426
Aaron Wells <chacewells@gmail.com>
2014-05-09 06:05:35 -0700

    wrote randomShuffledSubArray () function and added to qimport.js

--- js/qimport.js
+++ js/qimport.js
@@ -3,3 +3,24 @@ function shuffle(o){ //v1.0
     for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
 };
+
+// returns a shuffled subarray of subArraySize members from theArray
+function shuffledRandomSubArray (theArray, subArraySize) {
+	if (subArraySize > theArray.length)
+		return null;
+
+	var counter = 0;
+	var index;
+	var subArray = [];
+	
+	while (counter < subArraySize) {
+		index = Math.floor(Math.random() * theArray.length);
+		item = theArray[index];
+		if ($.inArray(item, subArray) < 0) {
+			subArray[counter] = theArray[index];
+			counter++;
+		}
+	}
+
+	return subArray;
+};
